d9e46c99f9f2a87f04e1e017665293d8265b96f2
Zaquariah Holland <Zaquariah@gmail.com>
2025-07-20 21:14:39 -0500

    feat: add rate limiting

--- contact-api/index.js
+++ contact-api/index.js
@@ -2,27 +2,37 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/contact', async (req, res) => {
+// Rate limiter middleware for contact form
+// TODO: consider environments (i.e. development vs production)
+const contactLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'Too many submissions from this IP, please try again later.',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false,  // Disable the `X-RateLimit-*` headers
+});
+
+app.post('/contact', contactLimiter, async (req, res) => {
   const { name, email, message } = req.body;
   try {
+    // captcha verification
     const token = req.body.captchaToken;
-    console.log('Received captchaToken:', token);
     const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${token}`;
 
     const captchaRes = await fetch(verifyUrl, { method: 'POST' });
     const captchaData = await captchaRes.json();
 
-    console.log('reCAPTCHA verification:', captchaData);
     if (!captchaData.success) {
       return res.status(400).send('reCAPTCHA failed');
     }
 
-
+    // Send Email
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
